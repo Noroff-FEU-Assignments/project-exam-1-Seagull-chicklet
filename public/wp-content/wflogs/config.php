48b3421d3146e509328554f4168c09b1b1dffb5e
Seagull-chicklet <126714065+Seagull-chicklet@users.noreply.github.com>
2024-01-25 14:08:49 +0100

    adding wordpress

---
+++ public/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1706549473;s:7:"authKey";s:64:"cf$G$=A!nUI0>1FV7Rgn`B%ccQe8KhD3X=wj=3xr{Jo..mYX5S-l/ h5jkdhZS7J";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:3593;}
