3d750aaebdd499ef6f1295b3f04ebbef493f66a0
Seagull-chicklet <126714065+Seagull-chicklet@users.noreply.github.com>
2024-02-01 10:56:13 +0100

    no connection to wp  searching solutions

--- public/wp-content/themes/mom-vs-real-world1/function.php
+++ public/wp-content/themes/mom-vs-real-world1/function.php
@@ -1,9 +1,7 @@
 <?php
-
-
-add_action( 'wp_enqueue_scripts', 'hubspot_blog_theme_enqueue_styles' );
-function hubspot_blog_theme_enqueue_styles() {
- wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  add_action( 'wp_enqueue_scripts', 'hubspot_blog_theme_enqueue_styles' );
+ function hubspot_blog_theme_enqueue_styles() {
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  wp_enqueue_style( 'child-style', get_template_directory_uri() . '/style.css' 
- array('parent-style'), wp_get_theme()->get('Version'));
+  array('parent-style'), wp_get_theme()->get('Version'));
 }
